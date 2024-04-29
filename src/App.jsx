e71db1ed5d789079a9eae933fce675bb5768a669
Cristian Diaz <diazcristian0210@gmail.com>
2024-04-29 20:35:42 -0300

    Primer ejercicio tp react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Container} from "react-bootstrap"
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        <h1>Hellow  World!</h1>
+      </Container>
+    </>
+  )
+}
+
+export default App
